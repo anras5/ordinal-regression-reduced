5b8d726e9d507571bfadbb8d9bcb355d7223b068
anras5 <anras1filip@gmail.com>
2025-03-03 12:25:36 +0100

    feat: run all preferences and update suptitles in plots

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
-N=5
 DIR="ceiling-structures"
 
 
-mkdir -p "./data/$DIR/output/preferences_$N/plots/heatmap"
-mkdir -p "./data/$DIR/output/preferences_$N/plots/lineplot"
-python3 main.py \
-  --input "./data/$DIR/dataset.csv" \
-  --output_dir "./data/$DIR/output/preferences_$N" \
-  --n_preferences "$N" \
-  --components 2 3 4 5 6 \
-  --points 2 4 6
+for N in {1..6}; do
+  mkdir -p "./data/$DIR/output/preferences_$N/plots/heatmap"
+  mkdir -p "./data/$DIR/output/preferences_$N/plots/lineplot"
+  python3 main.py \
+    --input "./data/$DIR/dataset.csv" \
+    --output_dir "./data/$DIR/output/preferences_$N" \
+    --n_preferences "$N" \
+    --components 2 3 4 5 6 \
+    --points 2 3 4 5 6
+done
