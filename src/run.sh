0fa1a394580d4bdf7ae87357024960c399576f7b
anras5 <anras1filip@gmail.com>
2025-04-18 14:19:36 +0200

    feat: update run sh with methods flag

--- src/run.sh
+++ src/run.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
 
-DIR="insulating-materials"
-DATASET_FILENAME="dataset30.csv"
-BASENAME="${DATASET_FILENAME%.*}"
+# configure these variables:
+DIR="countries-resilience"  # folder located in the data directory, indicating the dataset
+DATASET_FILENAME="dataset.csv"  # filename of the dataset located in DIR (could be original dataset.csv or synthetic)
 
-for N in {1..6}; do
+# script:
+BASENAME="${DATASET_FILENAME%.*}"
+for N in {1..2}; do
   mkdir -p "./data/$DIR/output-$BASENAME/preferences_$N/plots/heatmap"
   mkdir -p "./data/$DIR/output-$BASENAME/preferences_$N/plots/lineplot"
   python3 main.py \
     --input "./data/$DIR/$DATASET_FILENAME" \
     --output_dir "./data/$DIR/output-$BASENAME/preferences_$N" \
     --n_preferences "$N" \
-    --components 2 3 4 5 6 \
-    --points 2 3 4 5 6
+    --components 2 3 4 \
+    --points 2 3 4 \
+    --metrics nec pwi rai
 done
